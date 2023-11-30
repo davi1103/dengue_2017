ae2af0db09261fdad8bf80ae441e825811e3a857
davi1103 <139092515+davi1103@users.noreply.github.com>
2023-11-30 02:39:03 -0500

    Add files via upload

--- js/landing.js
+++ js/landing.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/js/dengue_cases_peru_2017_with_duplicates.json')
+    fetch('/js/dengue_cases_peru_2017_reordered.json')
         .then(response => response.json())
         .then(data => {
             const tabla = document.getElementById('casos-dengue');
-            tabla.innerHTML = '<tr><th>Departamento</th><th>Fecha</th><th>Personas contagiadas</th><th>Incidencia</th><th>Personas fallecidas</th></tr>';
+            tabla.innerHTML = '<tr><th>departamento</th><th>fecha</th><th>casos_actuales</th><th>Incidencia</th><th>Personas fallecidas</th></tr>';
             data.forEach(registro => {
                 tabla.innerHTML += `<tr>
-                    <td>${registro.Departamento}</td>
-                    <td>${registro.Fecha}</td>
-                    <td>${registro['Personas contagiadas']}</td>
-                    <td>${registro.Incidencia.toFixed(2)}</td>
-                    <td>${registro['Personas fallecidas']}</td>
+                    <td>${registro.departamento}</td>
+                    <td>${registro.fecha}</td>
+                    <td>${registro['casos_actuales']}</td>
+                    <td>${registro.incidencia.toFixed(2)}</td>
+                    <td>${registro['personas fallecidas']}</td>
                 </tr>`;
             });
         });
